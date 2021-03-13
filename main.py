151b5e9017819646e74f1cb3bc137a7d1446c5d2
OsoSuerte <69661970+OsoSuerte@users.noreply.github.com>
2021-03-13 16:16:48 -0700

    Add files via upload

--- main.py
+++ main.py
@@ -13,8 +13,6 @@ X = X.values.reshape(-1, 1)
 
 y = prod_per_year['totalprod']
 
-
-
 regr = LinearRegression()
 regr.fit(X, y)
 y_predict = regr.predict(X)
@@ -28,7 +26,16 @@ plt.plot(X, y_predict, 'o')
 plt.title("Honey")
 plt.xlabel("Year")
 plt.ylabel("Honey Production")
+
+'''print(regr.coef_) will give you slop of the line
+print(regr.intercept_) will give you intercept'''
+
+X_future = np.array(range(2013, 2051))
+X_future = X_future.reshape(-1, 1)
+
+future_predict = regr.predict(X_future)
+
+#plt.plot(X, y, 'o')
+plt.plot(X_future, future_predict, 'o')
 plt.show()
-
-
-
+print(future_predict[-1]) # This gives the last value (2050 honey production)
